030731f569404d684ce6271b3cd56781dc61ee35
chlbri <bri_lvi@icloud.com>
2025-01-28 13:54:09 +0000

    chore: scripts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import { globSync } from 'glob';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'rollup';
+import { circularDependencies } from 'rollup-plugin-circular-dependencies';
 import { nodeExternals } from 'rollup-plugin-node-externals';
 import tscAlias from 'rollup-plugin-tsc-alias';
 import typescript from 'rollup-plugin-typescript2';
@@ -43,6 +44,9 @@ export default defineConfig({
       tsconfigOverride: {
         exclude: ignore,
       },
+    }),
+    circularDependencies({
+      exclude: [],
     }),
 
     nodeExternals({
